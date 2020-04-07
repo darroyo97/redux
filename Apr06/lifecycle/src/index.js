0eca3f71e99cf39b77999ff7096fa6ecc30c2629
Daniela <daniela.arroyo27@gmail.com>
2020-04-07 10:09:20 -0500

    step one

--- Apr06/lifecycle/src/index.js
+++ Apr06/lifecycle/src/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
 
 //Create counter component 
+
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: "Hola"
+    }
+  }
+  render() {
+    return (
+      <>
+        {/* Goal is to increment the global state  */}
+        <h1>{this.state.title}</h1>
+        <span>{this.props.count}</span>
+
+        <button onClick={}>Increase</button>
+      </>
+    )
+  }
+
+}
 
 // Action Creators 
 // Reducers (state, action) 
