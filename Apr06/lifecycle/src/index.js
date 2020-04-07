b841318fb267dc8d6ddae534e9da6d2c3532613d
Daniela <daniela.arroyo27@gmail.com>
2020-04-07 10:04:21 -0500

    starting steps

--- Apr06/lifecycle/src/index.js
+++ Apr06/lifecycle/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
 
+//Create counter component 
+
+// Action Creators 
+// Reducers (state, action) 
+// create store (reducer)
+// dispatch 
+// mapStateToProps 
+// mapDispatchToProps 
+//connect component to connect mapStateToProps
+// and mapDispatchToProps
+//wrap our application inside Provider
+//pass to the provider the store
+
 
 
 ReactDOM.render(
