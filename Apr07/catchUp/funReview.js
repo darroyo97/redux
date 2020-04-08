ae49924e26dd874e4b9e0d280e7e9704a9a7b1e4
Daniela <daniela.arroyo27@gmail.com>
2020-04-07 21:52:18 -0500

    mini review

---
+++ Apr07/catchUp/funReview.js
@@ -0,0 +1,14 @@
+// two different way but same result
+
+
+//first way
+let sample = () => {
+    return 'hello world'
+}
+
+console.log(sample())
+
+// second way
+let sample2 = () => 'hello world'
+
+console.log(sample2())
