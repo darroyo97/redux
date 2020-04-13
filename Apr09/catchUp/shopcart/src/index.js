7a0c4d15df98e34f472831ab1431dc1a7b62aac8
Daniela <daniela.arroyo27@gmail.com>
2020-04-13 09:39:41 -0500

    set up

--- Apr09/catchUp/shopcart/src/index.js
+++ Apr09/catchUp/shopcart/src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux'
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux'
 import Cart from './components/Cart';
 import AddProduct from './components/AddProduct'
+import BaseLayout from './components/layout/BaseLayout'
 
 
 ReactDOM.render(
-  <Provider>
-    <BrowserRouter>
-      <BaseLayout>
-        <Switch>
-          <Route exact path="/" component={Cart} />
-          <Route exact path="/addProduct" component={AddProduct} />
-        </Switch>
-      </BaseLayout>
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <BaseLayout>
+      <Switch>
+        <Route exact path="/" component={Cart} />
+        <Route exact path="/addProduct" component={AddProduct} />
+      </Switch>
+    </BaseLayout>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
